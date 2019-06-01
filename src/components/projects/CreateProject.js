732424639cee625b4700ea8c68af4f5ac026592f
Lac Tran <trangialac.fin@gmail.com>
2019-06-01 12:14:35 +0300

    connected with redux

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createProject } from '../../store/actions/projectActions';
 
 class CreateProject extends Component {
     state = {
@@ -8,7 +10,8 @@ class CreateProject extends Component {
 
     handleOnSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        // console.log(this.state);
+        this.props.createProject(this.state);
     }
 
     handleOnChange = e => {
@@ -38,4 +41,12 @@ class CreateProject extends Component {
     }
 }
 
-export default CreateProject;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createProject: (project) => {
+            dispatch(createProject(project))
+        }
+    }
+}
+
+export default connect(null, mapDispatchToProps)(CreateProject);
---
+++ src/store/actions/projectActions.js
@@ -0,0 +1,14 @@
+export const createProject = (project) => {
+    // return {
+    //     type: 'ADD_PROJECT',
+    //     project: project
+    // }
+
+    return (dispatch, getState) => {
+        // make async call to database
+        dispatch({
+            type: 'CREATE_PROJECT',
+            project
+        })
+    }
+}
