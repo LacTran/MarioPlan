669a10fed82f85795010ec27bcc0e367baf4984a
Lac Tran <trangialac.fin@gmail.com>
2019-06-02 13:51:29 +0300

    working with fb functions and deployment

---
+++ dist/precache-manifest.aaa18d7267ed66822484e030c3135ba3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33bfaa528fd4aaedb315a9e18925617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8f1fa79baecdcfbfd5e",
+    "url": "/static/css/main.e1121f11.chunk.css"
+  },
+  {
+    "revision": "0a46373d75d4e0c441ed",
+    "url": "/static/js/2.f9241417.chunk.js"
+  },
+  {
+    "revision": "b8f1fa79baecdcfbfd5e",
+    "url": "/static/js/main.30ac8a1f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
